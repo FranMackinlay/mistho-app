01182cd91879010d03c02a0da167a9f79b8b08e4
Francisco Mackinlay <franmackinlay@gmail.com>
2021-05-26 22:49:36 +0200

    IMPROVE, login component

--- economist-scrapper/src/components/LoginComponent/LoginComponent.jsx
+++ economist-scrapper/src/components/LoginComponent/LoginComponent.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
+import React from 'react';
+
+import './LoginComponent.css';
 
 
 export default function LoginComponent() {
   return (
-    <div>
-
-    </div>
+    <div className='login'>
+				<h1>Welcome!</h1>
+				<form className="df fldc">
+					<input type='email' placeholder='Email..' />
+					<input type='password' placeholder='Password..' />
+					<button type='submit'>Login</button>
+				</form>
+			</div>
   )
 }
