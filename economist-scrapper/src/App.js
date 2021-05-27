e62d3b654db4d041cc65a3580c2fd4f46d4b77ab
Francisco Mackinlay <franmackinlay@gmail.com>
2021-05-27 20:42:18 +0200

    ADD, articles screen

--- economist-scrapper/src/App.js
+++ economist-scrapper/src/App.js
@@ -2,12 +2,14 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import './App.css';
 import LoginComponent from './components/LoginComponent/LoginComponent';
+import ArticlesScreen from './screens/ArticlesScreen/ArticlesScreen';
 
 function App() {
   return (
     <Router>
       <Switch>
         <Route path="/login" component={LoginComponent} />
+        <Route path="/articles" component={ArticlesScreen} />
         <Route to="/" component={LoginComponent} />
       </Switch>
     </Router >
---
+++ economist-scrapper/src/screens/ArticlesScreen/ArticlesScreen.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function ArticlesScreen() {
+  return (
+    <div>
+      Articles
+    </div>
+  )
+}
