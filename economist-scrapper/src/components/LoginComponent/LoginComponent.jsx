b211207ce3eaa97b637cb737f63c59165c2ff9d1
Francisco Mackinlay <franmackinlay@gmail.com>
2021-05-27 15:49:29 +0200

    WIP, login

--- economist-scrapper/src/components/LoginComponent/LoginComponent.jsx
+++ economist-scrapper/src/components/LoginComponent/LoginComponent.jsx
@@ -1,15 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import './LoginComponent.css';
 
 
 export default function LoginComponent() {
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const onChangeEmail = e => {
+    e?.preventDefault();
+    setEmail(e.target.value);
+  }
+
+  const onChangePassword = e => {
+    e?.preventDefault();
+    setPassword(e.target.value);
+  }
+
+  useEffect(() => {
+    console.log(`email, password`, email, password)
+  }, [email, password]);
+
   return (
     <div className='login'>
 				<h1>Welcome!</h1>
 				<form className="df fldc">
-					<input type='email' placeholder='Email..' />
-					<input type='password' placeholder='Password..' />
+					<input type='email' value={email} onChange={onChangeEmail} placeholder='Email..' />
+					<input type='password' value={password} onChange={onChangePassword} placeholder='Password..' />
 					<button type='submit'>Login</button>
 				</form>
 			</div>
