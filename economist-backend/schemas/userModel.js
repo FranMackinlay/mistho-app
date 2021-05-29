0b3219f220fca6cf4d2d967a53b9c96ef4953648
Francisco Mackinlay <franmackinlay@gmail.com>
2021-05-29 11:42:47 +0200

    ADD, name to userModel

--- economist-backend/schemas/userModel.js
+++ economist-backend/schemas/userModel.js
@@ -1,6 +1,12 @@
 import mongoose from 'mongoose';
 
 const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    trim: true,
+    required: true,
+    unique: true,
+  },
   email: {
     type: String,
     trim: true,
